ff99abc9a61b6a3630c8507147c796b7090db39c
Kaveena99 <kaveenadewminijayasekara@gmail.com>
2020-08-11 01:37:03 +0530

    Pharmacy Activities

--- app/src/main/java/com/example/echannelling_new/MainActivity.java
+++ app/src/main/java/com/example/echannelling_new/MainActivity.java
@@ -1,54 +1,31 @@
 package com.example.echannelling_new;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.MenuItem;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private Button button20;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.navigation);
-        bottomNavigationView.setSelectedItemId(R.id.home);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+        button20 = (Button) findViewById(R.id.button20);
+        button20.setOnClickListener(new View.OnClickListener(){
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()) {
-                    case R.id.home:
-                        return true;
-
-                    case R.id.logout:
-                        startActivity(new Intent(getApplicationContext(), Logout.class));
-                        finish();
-                        overridePendingTransition(0, 0);
-                        return true;
-
-                    case R.id.profile:
-                        startActivity(new Intent(getApplicationContext(), Profile.class));
-                        finish();
-                        overridePendingTransition(0, 0);
-                        return true;
-                }
-
-                return false;
+            public void onClick(View v){
+                openLogin();
             }
 
         });
     }
-
-
-
+    private void openLogin() {
+        Intent intent = new Intent (this, LoginActivity.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/echannelling_new/Profile.java
+++ app/src/main/java/com/example/echannelling_new/Profile.java
@@ -3,46 +3,38 @@ package com.example.echannelling_new;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Profile extends AppCompatActivity {
 
+    private BottomNavigationView textView;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.navigation);
-        bottomNavigationView.setSelectedItemId(R.id.profile);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+        textView = (BottomNavigationView) findViewById(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener(){
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()) {
-                    case R.id.home:
-                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        finish();
-                        overridePendingTransition(0, 0);
-                        return true;
-
-                    case R.id.logout:
-                        startActivity(new Intent(getApplicationContext(), Logout.class));
-                        finish();
-                        overridePendingTransition(0, 0);
-                        return true;
-
-                    case R.id.profile:
-                        return true;
-
-                }
-
-                return false;
+            public void onClick(View v){
+                openProfile();
             }
 
         });
+
+    }
+
+    private void openProfile() {
+        Intent intent = new Intent (this, profilePharmacy.class);
+        startActivity(intent);
     }
 }
